af3ff78da5aa551f179ee3a8a289ff9ff47261bf
wisnuyudis <wisnuyudis@sdt.co.id>
2026-08-28 10:24:12 +0700

    Revert "Merge pull request #1 from wisnuyudis/appsealing"
    This reverts commit 7fb887efe3160b59d738666812f8b5baf8d0f911, reversing
    changes made to 781daa24e157937ab174550702e24b4e079021fc.

--- App.tsx
+++ App.tsx
@@ -5,22 +5,14 @@
  */
 
 import React from 'react';
-import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { useSecurityAlert } from './src/state/useSecurityAlert';
 import { HomeScreen } from './src/ui/HomeScreen';
-import { SecuritySheet } from './src/ui/SecuritySheet';
-import { dark, light } from './src/ui/theme';
 
 function App() {
-  const scheme = useColorScheme();
-  const { alert, dismiss } = useSecurityAlert();
-
   return (
     <SafeAreaProvider>
       <HomeScreen />
-      <SecuritySheet alert={alert} palette={scheme === 'dark' ? dark : light} onDismiss={dismiss} />
     </SafeAreaProvider>
   );
 }
--- ios/Tickdown/CallRiskMonitor.m
+++
@@ -1,6 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface RCT_EXTERN_MODULE (CallRiskMonitor, RCTEventEmitter)
-
-@end
